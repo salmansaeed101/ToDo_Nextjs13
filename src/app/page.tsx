bbd4dc70bf7e3514ada9fe49730017d44d15936b
salman <salmansaeed101@gmail.com>
2023-01-31 10:41:47 +0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,60 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+'use client'
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import React, {useState} from 'react';
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
+export default function ToDo() {
+  const [todo, setTodo]=useState('')
+  const [todos,setTodos]=useState([
+    { todoText:"ToDo1",completed:false},
+    {todoText:"ToDo2",completed:true},
+    {todoText:"ToDo3",completed:true},
+    {todoText:"ToDo4",completed:false}
+  
+  ])
+  const onClickHandler = (meraElm:any)=>{
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    const newtodos = todos.map((todo)=>{
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
+if(todo.todoText == meraElm.todoText){
+  todo.completed = !todo.completed
+}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+return todo;
+    });
+setTodos(newtodos);
+  };
+  const addTodo =()=>{
+    const newTodo ={todoText:todo, completed:false}
+    const newTodos = [...todos, newTodo]
+    setTodos(newTodos)
+  };
+    const deleteTodo = (meraTodo:any)=>{
+      const newTodos = todos.filter(todo=>{
+        if (todo.todoText == meraTodo.todoText)
+        return false;
+        return true;
+      });
+      setTodos(newTodos)
+    }
+  
+  return ( 
+  <>
+  <div>ToDo</div>
+  <input placeholder='Add to do text' value={todo} onChange={(e)=>{setTodo(e.target.value)}} />
+  <button onClick={addTodo}>Add</button>
+  <ul>
+{todos.map((elm)=>{
+  return <li style={{color:elm.completed ?"blue":"green", fontStyle:"oblique",listStyle:"none"}}
+   key={elm.todoText}>
+    <input type="checkbox" checked={elm.completed} onChange={()=>{onClickHandler(elm)}} />
+    {elm.todoText}
+    <button onClick={()=>{deleteTodo(elm)}} > Delete ToDo </button>
+    </li>
+})}
+
+  </ul>
+  </>
+    
   )
 }
